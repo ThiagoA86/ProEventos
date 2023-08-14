fa2b0af41cb14d722f2582085dd01d8197a4f2dd
Thiago Alves Pereira <thiago.data_consult@hotmail.com>
2023-08-14 14:05:22 -0300

    Seção 4 - Angular

---
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PalestranteComponent } from './palestrante/palestrante.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+      PalestranteComponent,
+      EventosComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
