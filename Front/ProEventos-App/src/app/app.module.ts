45ec68637ffacbb01a929b8ff3cc22c2b0f70890
Thiago Alves Pereira <thiago.data_consult@hotmail.com>
2023-08-16 13:00:12 -0300

    Seção 5 - Anuglar Interpolação

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -8,18 +8,24 @@ import { PalestranteComponent } from './palestrante/palestrante.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './Nav/Nav.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
       PalestranteComponent,
-      EventosComponent
+      EventosComponent,
+      NavComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,12 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
-  public eventos:any;
+  public eventos:any=[];
+  public eventosFiltrados:any=[];
+  widthImg:number=100;
+  marginImg:number=2;
+  mostrarImagem:boolean=true;
+  private _filtroLista:string='';
+  public get filtroLista(){
+    return this._filtroLista;
+  }
+  public set filtroLista(value:string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista):this.eventos;
+  }
+  filtrarEventos(filtraPor:string):any{
+    filtraPor = filtraPor.toLowerCase();
+    return this.eventos.filter(
+      (      evento: { tema: string;local:string })=>evento.tema.toLowerCase().indexOf(filtraPor)!==-1 ||
+      evento.local.toLowerCase().indexOf(filtraPor)!==-1
+    );
+  }
   constructor(private http:HttpClient){}
-
+  alterarImagem(){
+    this.mostrarImagem = !this.mostrarImagem;
+  }
   public GetEventos():void{
     this.http.get('https://localhost:5001/api/Eventos').subscribe(
-      response=>this.eventos=response,
+      response=>{
+        this.eventos=response;
+        this.eventosFiltrados=this.eventos;
+      },
       error=>console.log(error)
     );
 
